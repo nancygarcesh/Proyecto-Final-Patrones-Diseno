81f1ddc0a65c43aafb5db2e576f43060233fab86
nancygarcesh <nghazou35@gmail.com>
2024-05-08 10:21:23 -0400

    Composite desarrollado en el proyecto

---
+++ ProyectoFinalPD/Composite/PaqueteCompuesto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalPD.Composite
+{
+    public class PaqueteCompuesto : IPaqueteHabitacion
+    {
+        private List<IPaqueteHabitacion> hijos = new List<IPaqueteHabitacion>();
+        private double descuento;
+
+        public void Agregar(IPaqueteHabitacion paquete)
+        {
+            hijos.Add(paquete);
+        }
+
+        public void Remover(IPaqueteHabitacion paquete)
+        {
+            hijos.Remove(paquete);
+        }
+
+        public double ObtenerPrecio()
+        {
+            double precioTotal = 0;
+            foreach (var paquete in hijos)
+            {
+                precioTotal += paquete.ObtenerPrecio();
+            }
+            return precioTotal * (1 - descuento);
+        }
+
+        public string ObtenerDescripcion()
+        {
+            string descripcion = "";
+            foreach (var paquete in hijos)
+            {
+                descripcion += paquete.ObtenerDescripcion() + "\n";
+            }
+            return descripcion;
+        }
+
+        public void EstablecerDescuento(double descuento)
+        {
+            this.descuento = descuento;
+        }
+    }
+}
--- ProyectoFinalPD/Program.cs
+++ ProyectoFinalPD/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoFinalPD.Composite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,58 @@ namespace ProyectoFinalPD
     {
         static void Main(string[] args)
         {
+            Cliente cliente = new Cliente();
+
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("Seleccione una opción:");
+                Console.WriteLine("1. Reservar habitación individual (ONE)");
+                Console.WriteLine("2. Reservar paquete Medium (MEDIUM)");
+                Console.WriteLine("3. Reservar paquete Big (BIG)");
+                Console.WriteLine("4. Salir");
+
+                int opcion;
+                if (int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            cliente.HacerReservacion(new HabitacionIndividual(100, "Habitación individual (ONE)"));
+                            break;
+                        case 2:
+                            var paqueteMedium = new PaqueteCompuesto();
+                            paqueteMedium.Agregar(new HabitacionIndividual(200, "Paquete Medium (MEDIUM)"));
+                            paqueteMedium.Agregar(new HabitacionIndividual(200, "Paquete Medium (MEDIUM)"));
+                            paqueteMedium.EstablecerDescuento(0.1); // Descuento del 10%
+                            cliente.HacerReservacion(paqueteMedium);
+                            break;
+                        case 3:
+                            var paqueteBig = new PaqueteCompuesto();
+                            paqueteBig.Agregar(new HabitacionIndividual(100, "Habitación individual (ONE)"));
+                            paqueteBig.Agregar(new HabitacionIndividual(200, "Paquete Medium (MEDIUM)"));
+                            paqueteBig.Agregar(new HabitacionIndividual(200, "Paquete Medium (MEDIUM)"));
+                            paqueteBig.EstablecerDescuento(0.2); // Descuento del 20%
+                            cliente.HacerReservacion(paqueteBig);
+                            break;
+                        case 4:
+                            continuar = false;
+                            break;
+                        default:
+                            Console.WriteLine("Opción no válida. Por favor, seleccione una opción válida.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada no válida. Por favor, introduzca un número.");
+                }
+
+                Console.WriteLine("¿Desea realizar otra acción? (s/n)");
+                string respuesta = Console.ReadLine();
+                continuar = respuesta.Equals("s", StringComparison.OrdinalIgnoreCase);
+            }
+            Console.ReadLine();
         }
     }
 }
